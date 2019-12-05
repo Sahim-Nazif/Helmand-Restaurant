0dbfcc25131e3872f0eb90db82e85aac6b8e12cc
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2019-12-04 20:56:04 -0500

    add view for Menu Item

--- Helmand/Areas/Admin/Controllers/MenuItemController.cs
+++ Helmand/Areas/Admin/Controllers/MenuItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Helmand.Data;
+using Helmand.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,15 +17,36 @@ namespace Helmand.Areas.Admin.Controllers
         //whenever we need to save anything on the server or we need the rout path of the app we need Webhosting
         private readonly IWebHostEnvironment _webHostEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemView { get; set; }
+
         public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
             _webHostEnvironment = webHostEnvironment;
+            MenuItemView = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                //here we don't need to attach subcategory, since it will based on what category gets selected
+                MenuItem=new Models.MenuItem()
+            };
+
         }
+        //Get-Action Method
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var menuitems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
             return View(menuitems);
         }
+
+        //Adding menu-item to db
+
+        public IActionResult AddMenuItem()
+        {
+            return View(MenuItemView);
+        }
+
+
     }
 }
