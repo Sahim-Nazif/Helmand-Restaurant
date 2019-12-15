00b0b6f10526a5476cdba0e66aeec03e07892c97
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2019-12-15 11:51:42 -0500

    Delete View & Get IActionResult added

--- Helmand/Areas/Admin/Controllers/CouponController.cs
+++ Helmand/Areas/Admin/Controllers/CouponController.cs
@@ -74,5 +74,67 @@ namespace Helmand.Areas.Admin.Controllers
 
 
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>EditCoupon(Coupon coupon)
+        {
+
+            if (coupon.Id == 0)
+            {
+                return NotFound();
+            }
+
+            var couponFromDB = await _db.Coupon.Where(c => c.Id == coupon.Id).FirstOrDefaultAsync();
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                //if it is greater than zeros means the file is uploaded
+                if (files.Count > 0)
+                {
+                    //then will convert this to a stream of bytes to store it in the database
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    couponFromDB.Picture = p1;
+                }
+                couponFromDB.Name = coupon.Name;
+                couponFromDB.MinimumAmount = coupon.MinimumAmount;
+                couponFromDB.Discount = coupon.Discount;
+                couponFromDB.CouponType = coupon.CouponType;
+                couponFromDB.IsActive = coupon.IsActive;
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(coupon);
+        }
+
+        //Delete get
+
+        public async Task<IActionResult> deleteCoupon(int ?id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var getCoupon = await _db.Coupon.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (getCoupon == null)
+            {
+                return NotFound();
+            }
+            return View (getCoupon);
+        }
+
+        //Delete Post 
+
     }
 }
