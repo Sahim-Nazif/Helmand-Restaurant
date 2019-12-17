57ebb383749e11cf3065219c9271f111597500fd
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2019-12-16 21:29:52 -0500

    Sd class updated to properties for different User Roles

--- Helmand/Utility/SD.cs
+++ Helmand/Utility/SD.cs
@@ -8,5 +8,10 @@ namespace Helmand.Utility
     public static class SD
     {
         public const string DefaultFoodImage = "default_food.png";
+        //different roles for the user to be able to use the application
+        public const string ManagerUser = "Manager";
+        public const string KitchenUser = "Kitchen";
+        public const string FrontDeskUser = "FrontDesk";
+        public const string CustomerEndUser="Customer";
     }
 }
