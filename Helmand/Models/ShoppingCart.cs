d6bad8715f281b9072a571bcdabac99b8b647b6d
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2019-12-22 14:12:04 -0500

    Created ShoppingCart Model

--- Helmand/Models/ShoppingCart.cs
+++ Helmand/Models/ShoppingCart.cs
@@ -7,5 +7,9 @@ namespace Helmand.Models
 {
     public class ShoppingCart
     {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        public int MenuItemId { get; set; }
+        public int Count { get; set; }
     }
 }
