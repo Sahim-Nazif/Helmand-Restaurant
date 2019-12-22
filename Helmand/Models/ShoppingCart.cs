11cb771ad97eb7fe85894ae3373e88a9a5f86452
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2019-12-22 14:18:31 -0500

    Added foreignkey to shoppingCart to  reference MenuItem & ApplicationUser models

--- Helmand/Models/ShoppingCart.cs
+++ Helmand/Models/ShoppingCart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,16 @@ namespace Helmand.Models
     {
         public int Id { get; set; }
         public string ApplicationUserId { get; set; }
+        
+        //notmapped is used when we don't to add any field to database
+        [NotMapped]
+        [ForeignKey("ApplicationUserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
         public int MenuItemId { get; set; }
+        [NotMapped]
+        [ForeignKey(" MenuItemId")]
+        public MenuItem MenuItem { get; set; }
         public int Count { get; set; }
     }
 }
