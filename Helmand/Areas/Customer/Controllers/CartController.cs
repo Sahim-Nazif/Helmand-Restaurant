3505246e41b83562ed2132c961fb1a73e42c9d9b
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2019-12-26 10:24:21 -0500

    Created CartController

---
+++ Helmand/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Helmand.Data;
+using Helmand.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Helmand.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public OrderDetailsCart OrderDetailsCartVM { get; set; }
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            OrderDetailsCartVM = new OrderDetailsCart()
+            {
+                OrderHeader = new Models.OrderHeader()
+            };
+
+
+            //in order to calculate the shopping cart total, we to set initialize it to zero
+
+            OrderDetailsCartVM.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+
+
+            return View();
+        }
+    }
+}
