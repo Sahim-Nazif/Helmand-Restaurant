fe913b7496971859a4b7e2ccee32f1650749ab57
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2019-12-27 11:56:58 -0500

    added Jquery for date and time picking on Cart Summary page

--- Helmand/Areas/Customer/Controllers/CartController.cs
+++ Helmand/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Helmand.Data;
+using Helmand.Models;
 using Helmand.Models.ViewModels;
 using Helmand.Utility;
 using Microsoft.AspNetCore.Http;
@@ -60,6 +61,56 @@ namespace Helmand.Areas.Customer.Controllers
                 }
             }
             OrderDetailsCartVM.OrderHeader.OrderTotalOriginal = OrderDetailsCartVM.OrderHeader.OrderTotal;
+
+            if (HttpContext.Session.GetString(SD.ssCouponCode) != null)
+            {
+                OrderDetailsCartVM.OrderHeader.CouponCode = HttpContext.Session.GetString(SD.ssCouponCode);
+                var couponFromDb = await _db.Coupon.Where(c => c.Name.ToLower() == OrderDetailsCartVM.OrderHeader.CouponCode.ToLower()).FirstOrDefaultAsync();
+                OrderDetailsCartVM.OrderHeader.OrderTotal = SD.DiscountedPrice(couponFromDb, OrderDetailsCartVM.OrderHeader.OrderTotalOriginal);
+
+            }
+
+            return View(OrderDetailsCartVM);
+        }
+        //Shopping Cart Summary
+        public async Task<IActionResult> Summary()
+        {
+            OrderDetailsCartVM = new OrderDetailsCart()
+            {
+                OrderHeader = new Models.OrderHeader()
+            };
+
+
+            //in order to calculate the shopping cart total, will initialize OrderTotal to zero
+
+            OrderDetailsCartVM.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ApplicationUser applicationUser = await _db.ApplicationUser.Where(c => c.Id == claim.Value).FirstOrDefaultAsync();
+
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+
+            if (cart != null)
+            {
+                OrderDetailsCartVM.listCart = cart.ToList();
+            }
+            // to calculate the order total
+
+            foreach (var list in OrderDetailsCartVM.listCart)
+            {
+                list.MenuItem = await _db.MenuItem.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
+
+                OrderDetailsCartVM.OrderHeader.OrderTotal = OrderDetailsCartVM.OrderHeader.OrderTotal + (list.MenuItem.Price * list.Count);
+               
+            }
+
+            OrderDetailsCartVM.OrderHeader.OrderTotalOriginal = OrderDetailsCartVM.OrderHeader.OrderTotal;
+
+            OrderDetailsCartVM.OrderHeader.PickupName = applicationUser.FirstName;
+            OrderDetailsCartVM.OrderHeader.PhoneNumber = applicationUser.PhoneNumber;
+            OrderDetailsCartVM.OrderHeader.PickUpTime = DateTime.Now;
 
             if (HttpContext.Session.GetString(SD.ssCouponCode) != null)
             {
