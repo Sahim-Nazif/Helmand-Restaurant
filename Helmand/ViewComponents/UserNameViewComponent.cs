eb2747653f9639b533fef2af23f582b41ebdedbf
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-01-01 11:22:26 -0500

    Created UsernameViewComponent Class

---
+++ Helmand/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Helmand.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helmand.ViewComponents
+{
+    public class UserNameViewComponent:ViewComponent
+    {
+        //first will have create object our DB in order to get the user who is logged in
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+
+        }
+    }
+}
