bd6ea0f572a413dfd74962f50102764c08fd7447
Nazifullah Sahim <nsahim@my.centennialcollege.ca>
2020-01-01 12:32:56 -0500

    added IviewcomponentResult method to UserNameViewComponent class

--- Helmand/ViewComponents/UserNameViewComponent.cs
+++ Helmand/ViewComponents/UserNameViewComponent.cs
@@ -1,8 +1,10 @@
 ﻿using Helmand.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Helmand.ViewComponents
@@ -14,6 +16,18 @@ namespace Helmand.ViewComponents
 
         public UserNameViewComponent(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //will get the user identity 
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Id == claim.Value);
+
+            return View(userFromDb); 
 
         }
     }
